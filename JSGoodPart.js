cf9480715952780e60b095c3c8f36667d5c62a0a
Compasses <gnhe2009@gmail.com>
2015-01-21 16:37:01 +0800

    Description: continue

--- JSGoodPart.js
+++ JSGoodPart.js
@@ -219,7 +219,6 @@ if  (typeof Object.create !== 'function') {
 		return new F();
 	}
 }
-
 var st1 = Object.create(new Object);
 st1.firstname = "okokok";
 
@@ -478,3 +477,128 @@ println(seqer);
 seqer.set_prefix('Q');
 seqer.set_seq(1000);
 println(seqer.gensym());
+
+/*
+
+	var constructor = function (spec, my) {
+		var that, other private instance variables;
+		my = my || {};
+		Add shared variables and functions to my
+		that = a new object;
+		Add privileged methods to that
+		return that;
+	};
+*/
+
+var mammal = function (spec) {
+	var that = {};
+
+	that.get_name = function () {
+		return spec.name;
+	};
+
+	that.says = function () {
+		return spec.saying || '';
+	};
+
+	return that;
+}
+var myMammal = mammal({name: 'Herb'});
+println(myMammal.get_name());
+
+var cat = function (spec) {
+	spec.saying = spec.saying || 'meow';
+
+	var that = mammal(spec);
+	that.purr  = function (n) {
+		var i, s='';
+		for (i = 0; i < n; ++i) {
+			if (s) {
+				s+= '-';
+			}
+			s += 'r';
+		}
+		return s;
+	}
+	that.get_name = function () {
+		return that.says() + ' ' + spec.name + ' ' + that.says();
+	}
+
+	return that;
+}
+
+var myCat = cat({name:'Henrietta'});
+
+println(myCat.get_name());
+println(myCat.purr(10));
+
+Object.method('superior', function (name) {
+	var that = this,
+		method = that[name];
+	return function () {
+		return method.apply(that, arguments);
+	}
+});
+
+
+
+var eventuality = function (that) {
+	var registry = {};
+
+	that.fire = function (event) {
+		// Fire an event on an object, the event can be either a string containing the name of the event
+		//or an object containing a type property containing the name of the event. Handlers registered by the 
+		// 'on' method that match the event name will be invoked.
+		var array,
+			func,
+			handler,
+			i,
+			type = typeof event === 'string' ? event : event.type;
+
+		// If an array of handlers exist for this event, then loop through it and execute the handlers in order
+		if (registry.hasOwnProperty(type)) {
+			array = registry[type];
+			for (i = 0; i < array.length; i += 1) {
+				handler = array[i];
+				//A handler record contains a method and an optional array of parameters. If the method is a name
+				// look up the function
+				func = handler.method;
+				if (typeof func === 'string') {
+					func = this[func];
+				}
+				//invoke a handler. If the record contained parameters, then pass them. Otherwise, pass the event objec
+				func.apply(this, handler.parameters || [event]);
+			}
+		}
+		return this;
+	};
+	that.on = function (type, method, parameters) {
+		//register an event, make a handler record. put it in a handler array, making one if doesn't yet exist for this type
+		var handler = {
+			method: method,
+			parameters: parameters
+		};
+		if (registry.hasOwnProperty(type)) {
+			registry[type].push(handler);
+		} else {
+			registry[type] = [handler];
+		}
+		return this;
+	};
+
+	return that;
+}
+
+var coolcat = function (spec) {
+	var that = cat(spec),
+		super_get_name = that.superior('get_name');
+	that.get_name = function(n) {
+		return 'like ' + super_get_name() + ' baby';
+	};
+	eventuality(that);
+	return that;
+}
+
+var myCoolCat = coolcat({name: 'bix'});
+
+println(myCoolCat.get_name());
